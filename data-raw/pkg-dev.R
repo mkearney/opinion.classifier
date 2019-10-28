5ffa017a92a049a7aabd0bde47540fd21e1583cd
mkearney <kearneymw@missouri.edu>
2019-10-28 12:59:10 -0500

    Edit pkg-dev

--- data-raw/pkg-dev.R
+++ data-raw/pkg-dev.R
@@ -1,3 +1,12 @@
 ## code to prepare `pkg-dev` dataset goes here
 
-usethis::use_data(pkg-dev, overwrite = TRUE)
+usethis::use_data_raw("pkg-dev")
+usethis::use_data_raw("nyt-get")
+usethis::use_data_raw("nyt-wrangle")
+usethis::use_mit_license("Michael W. Kearney")
+usethis::use_readme_rmd(open = FALSE)
+usethis::use_lifecycle_badge("experimental")
+usethis::use_cran_badge()
+usethis::use_git()
+usethis::use_github()
+usethis::use_travis()
